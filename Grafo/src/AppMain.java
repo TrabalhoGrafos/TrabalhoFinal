1b36cd9afa92e5c52555c89925d80be706f90558
Dennis <kafferdennis@gmail.com>
2014-06-26 08:55:59 -0300

    leitura do arquivo finalizada...

--- Grafo/src/AppMain.java
+++ Grafo/src/AppMain.java
@@ -5,9 +5,8 @@ import java.util.*;
 public class AppMain {
 
 	static Scanner sc = new Scanner(System.in);
-	private static BufferedReader br;
-	static ArrayList<Vertice> listaVertices = new ArrayList<Vertice>();
-
+	static ArrayList<Vertice<?>> listaVertices = new ArrayList<Vertice<?>>();
+	static ArrayList<Aresta> listaArestas = new ArrayList<Aresta>();
 	public static void main(String [] args){
 		
 		try {
@@ -19,7 +18,11 @@ public class AppMain {
 		
 		System.out.println(listaVertices.size());
 		
-		    System.out.println(listaVertices);
+		System.out.println(listaVertices);
+		
+        System.out.println(listaArestas.size());
+		
+		System.out.println(listaArestas);
 	}
 	
 	/**
@@ -32,38 +35,53 @@ public class AppMain {
 			
 			 String currentLine = reader.next();
 			
-			while(reader.hasNext()){
-				
-				currentLine = reader.next();
-				
-				Vertice<?> currentVertice = new Vertice<>();
-				
-				//System.out.println(currentLine);
+			 boolean read1 = true;
+			 boolean read2 = false;
 			
-				    int currentID = Integer.parseInt(currentLine);
+			while(reader.hasNext())
+			{
+				Aresta currentAresta = new Aresta();
+				
+				 if(!reader.hasNext("arestas")&&read1){
+					Vertice<?> currentVertice = new Vertice<>();
 				    currentLine = reader.next();
-					double currentX = Double.parseDouble(currentLine);
+				    System.out.println(currentLine);
+			
+				    currentVertice.setID(Integer.parseInt(currentLine));
+				    currentLine = reader.next();
+				    currentVertice.setCoordenadaX(Double.parseDouble(currentLine));
 					currentLine = reader.next();
-					double currentY = Double.parseDouble(currentLine);
-					
-					currentVertice.setID(currentID);
-					currentVertice.setCoordenadaX(currentX);
-					currentVertice.setCoordenadaY(currentY);
+					currentVertice.setCoordenadaY(Double.parseDouble(currentLine));
 					
 					listaVertices.add(currentVertice);
 				
-				if(reader.hasNext("arestas")){
-					currentLine = reader.next();
-					break;
-				
 				}
+				  if(reader.hasNext("arestas")){
+					read1 = false;
+					currentLine = reader.next();
+					read2 = true;
+						
+				}
+				else if(read2)
+				{
+					currentLine = reader.next();
+					Vertice<?> vertice1 = new Vertice<>();
+					Vertice<?> vertice2 = new Vertice<>();
+					
+					vertice1.setID(Integer.parseInt(currentLine));
+					currentAresta.origem = vertice1;
+					currentLine = reader.next();
+					vertice2.setID(Integer.parseInt(currentLine));
+					currentAresta.destino = vertice2;
+					currentLine = reader.next();
+					currentAresta.setCusto(Double.parseDouble(currentLine));
+					
+					listaArestas.add(currentAresta);
+					
+				}		
 			}	
 
 			fileReader.close();
 			reader.close();
-
 		}
-	
-	
-	
 }
--- Grafo/src/Aresta.java
+++ Grafo/src/Aresta.java
@@ -1,4 +1,30 @@
 
 public class Aresta {
-
+	private double custo;
+	public Vertice<?> origem;
+	public Vertice<?> destino;
+	
+	
+	public double getCusto() {
+		return custo;
+	}
+	public void setCusto(double custo) {
+		this.custo = custo;
+	}
+	public Vertice<?> getOrigem() {
+		return origem;
+	}
+	public void setOrigem(Vertice<?> origem) {
+		this.origem = origem;
+	}
+	public Vertice<?> getDestino() {
+		return destino;
+	}
+	public void setDestino(Vertice<?> destino){
+		this.destino = destino;
+	}
+	public String toString(){
+		return this.custo+" "+this.origem.getID()+" "+this.destino.getID()+"\n";
+	}
+	
 }
